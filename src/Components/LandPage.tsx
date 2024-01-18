c7f561a1a71c841d5f4ad031eeb4cc6b393aa08c
Saianjan <saianjan1234@gmail.com>
2024-01-18 13:12:07 +0530

    ADD: Created File Structure

---
+++ src/Components/LandPage.tsx
@@ -0,0 +1,13 @@
+function LandPage() {
+    return(
+        <div className="flex flex-col items-center">
+            <div>
+                <h1>Hi</h1>
+            </div>
+            <div>
+                <h1>Adding File Structure</h1>
+            </div>
+        </div>
+    )       
+}
+export default LandPage
